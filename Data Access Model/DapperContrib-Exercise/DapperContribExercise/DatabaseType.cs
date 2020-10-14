12866352dd67bf869e3391e65b436e657a48f45e
stonewm <stone.wangmin@qq.com>
2020-10-14 17:40:16 +0800

    2020/10/14

---
+++ Data Access Model/DapperContrib-Exercise/DapperContribExercise/DatabaseType.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DapperContribExercise {
+    public enum DatabaseType {
+        SqlServer,  // SQLServer数据库
+        MySql,      // Mysql数据库        
+        Sqlite      // SQLite数据库
+    }
+}

b742d4b781db65cfb17d300f6f7c87376b08af38
stonewm <stone.wangmin@hotmail.com>
2021-05-29 23:08:28 +0800

    Asp.net core OData Hello world

---
+++ AspNetCore - OData/AspNetCoreODataHello/AspNetCoreODataHello/Controllers/StudentsController.cs
@@ -0,0 +1,33 @@
+﻿using AspNetCoreODataHello.Models;
+using Microsoft.AspNet.OData;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+
+namespace AspNetCoreODataHello.Controllers
+{
+    public class StudentsController : ControllerBase
+    {
+        [HttpGet]
+        [EnableQuery()]
+        public IEnumerable<Student> Get()
+        {
+            return new List<Student>
+            {
+                new Student
+                {
+                    Id = Guid.NewGuid(),
+                    Name = "Vishwa Goli",
+                    Score = 100
+                },
+                new Student
+                {
+                    Id = Guid.NewGuid(),
+                    Name = "Josh McCall",
+                    Score = 120
+                }
+            };
+        }        
+    }
+}
+
